9e1d63ef73177bc9653d73fd94c6e7c1cd99c137
Jesse Potter <jessepottertest@gmail.com>
2013-03-16 09:04:46 -0700

    Replaced FireBase storage with local disk storage
    Created a new local storage API and swapped it in for the old FireBase
    API.  Now the data persists on the local machine and no internet
    connection is required.

---
+++ App3Navulus/bin/Debug/AppX/js/data/local-storage-api.js
@@ -0,0 +1,51 @@
+﻿var localDiskStorage =
+    {
+        baseFileName: 'data_',
+        baseFileExtension: '.dat',
+        localFolder: Windows.Storage.ApplicationData.current.localFolder,
+
+
+        getFileName: function (node) {
+            return localDiskStorage.baseFileName + node + localDiskStorage.baseFileExtension;
+        },
+
+        //Saves the data to the local disk
+        save: function (data, node, callback) {
+            var fileName = localDiskStorage.getFileName(node);
+            var fileText = JSON.stringify(data);
+
+            localDiskStorage.localFolder.createFileAsync(fileName, Windows.Storage.CreationCollisionOption.replaceExisting)
+            .then(function (saveFile) {
+                return Windows.Storage.FileIO.writeTextAsync(saveFile, fileText);
+            }).done(function () {
+                callback();
+            },
+            function (err) {
+                localDiskStorage.errorEvent(err);
+            });
+        },
+
+        //Loads data from local disk
+        load: function (node, callback) {
+            var fileName = localDiskStorage.getFileName(node);
+
+            localDiskStorage.localFolder.getFileAsync(fileName)
+            .then(function (saveFile) {
+                return Windows.Storage.FileIO.readTextAsync(saveFile);
+            }).done(function (data) {
+                var jsonData = JSON.parse(data);
+                callback(jsonData);
+            }, function (err) {
+                callback(null);
+                //localDiskStorage.errorEvent(err);
+            });
+        },
+
+        //Event that fires when the API has an error
+        errorEvent: function (err) {
+            var md = new Windows.UI.Popups.MessageDialog(err);
+            md.showAsync();
+            //document.getElementById('debug').innerHTML = err;
+        }
+
+    };
